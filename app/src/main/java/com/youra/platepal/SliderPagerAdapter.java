e4b821eba62a922447b1067c1c2590f504cabab2
Yurii Soliar <us1585707@gmail.com>
2023-03-27 03:20:28 +0300

    add intro

---
+++ app/src/main/java/com/youra/platepal/SliderPagerAdapter.java
@@ -0,0 +1,18 @@
+package com.youra.platepal;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+public class SliderPagerAdapter extends FragmentPagerAdapter {
+    public SliderPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+    @NonNull @Override public Fragment getItem(int position) {
+        return MainSliderfragment.newInstance(position);
+    }
+    // size is hardcoded
+    @Override public int getCount() {
+        return 4;
+    }
+}
